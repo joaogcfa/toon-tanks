0b9ff187db9b25110e0db9531c8093a2e285e022
joaogcfa <joaogcfa@al.insper.edu.br>
2025-02-10 07:17:16 -0300

    feat: add ToonTanksGameMode class and update configuration for game mode integration

--- ToonTanks/Source/ToonTanks/Projectile.cpp
+++ ToonTanks/Source/ToonTanks/Projectile.cpp
@@ -44,5 +44,6 @@ void AProjectile::OnHit(UPrimitiveComponent *HitComp, AActor *OtherActor, UPrimi
 	if (OtherActor && OtherActor != this && OtherActor != MyOwner)
 	{
 		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwnerInstigator, this, DamageTypeClass);
+		Destroy();
 	}
 }
